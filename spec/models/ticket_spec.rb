61b7a6738609dddd39376993b6dd42fa70537d3c
xavi <xabi1309@gmail.com>
2020-09-28 21:13:38 +0200

    Update ticket model test

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,17 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-  describe 'when required fields are valid' do
-    it 'successfully creates an ticket type' do
-      expect { FactoryBot.create(:ticket) }.to change { Ticket.count }.from(0).to(1)
+  context 'validations' do
+    describe 'when required fields are valid' do
+      it 'successfully creates a ticket' do
+        expect { FactoryBot.create(:ticket) }.to change { Ticket.count }.from(0).to(1)
+      end
+
+      it 'has an event associated' do
+        event = FactoryBot.create(:event, name: 'custom event')
+        ticket = FactoryBot.create(:ticket, event: event)
+
+        expect(ticket.event).to_not be_nil
+        expect(ticket.event.name).to eq(event.name)
+      end
     end
 
-    it 'has an event associated' do
-      event = FactoryBot.create(:event, name: 'custom event')
-      ticket = FactoryBot.create(:ticket, event: event)
+    describe 'when required fields are not valid' do
+      it 'does not create a ticket' do
+        ticket = FactoryBot.build(:ticket, event: nil)
+        expect { ticket.save }.to_not(change { Ticket.count })
+      end
+    end
+  end
 
-      expect(ticket.event).to_not be_nil
-      expect(ticket.event.name).to eq(event.name)
+  context '#update_available_tickets' do
+
+    let!(:ticket) { FactoryBot.create(:ticket, quantity: 10) }
+
+    xit 'updates available ticket amount' do
+
     end
   end
 end
